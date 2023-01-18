422d7887447b856cc0b0bc9888f6a3652c8aef54
Samuel Lopes <samuelhslopes@gmail.com>
2023-01-18 15:36:18 -0300

    Ajeitando a ordem das aulas

--- CursoJS/Aula9/indes.js
+++ CursoJS/Aula9/indes.js
@@ -7,3 +7,26 @@ let nome2 = "hehe"; // você pode declarar e re-declarar com var, já com let vo
 let nome2 = "hehe2";
 
 console.log(nome2);
+
+// ###########################################################  ###########################################################
+
+// Tipos primitivos String, Number, Undefined, null, boolean e symbol.
+
+const nome3 = 'Luiz'; // String.
+const num = 10; // Number.
+const num2 = 10.52; // Number.
+let nomeAluno; // Undefined = não aponta pra local nenhum na memória.
+let sobrenomeAluno = null; // NuloNão aponta pra local nenhum na memória.
+const aprovado = true; // Boolean = true ou false (Lógico).
+
+console.log(typeof nome3, num, num2, nomeAluno, sobrenomeAluno, aprovado);
+
+const a = [1, 2];
+const b = a;
+
+console.log(a, b);
+console.log(typeof a, b);
+
+b.push(3);
+console.log(a);
+console.log(b);
